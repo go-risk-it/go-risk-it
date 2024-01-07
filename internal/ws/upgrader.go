63b3db4e350e4e35e08196cc3797ae5b096a1fd0
Francesco <tomaselli.fr@gmail.com>
2024-01-07 11:17:44 +0100

    Json marshal and unmarshal demo

--- internal/ws/upgrader.go
+++ internal/ws/upgrader.go
@@ -1,27 +1,63 @@
 package ws
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/lesismal/nbio/nbhttp/websocket"
 	"go.uber.org/zap"
 )
+
+type Message struct {
+	PlayerId int
+	GameId   int
+	Payload  Payload
+}
+
+type Payload struct {
+	StartRegionId int
+	EndRegionId   int
+	NumTroops     int
+}
 
 func NewUpgrader(logger *zap.SugaredLogger) *websocket.Upgrader {
 	u := websocket.NewUpgrader()
 	u.OnOpen(func(c *websocket.Conn) {
 		// echo
 		logger.Info("OnOpen:", zap.String("remoteAddress", c.RemoteAddr().String()))
-		c.WriteMessage(1, []byte("Stato iniziale"))
+		c.WriteMessage(1, []byte("Established connection"))
 	})
+
 	u.OnMessage(func(c *websocket.Conn, messageType websocket.MessageType, data []byte) {
 		// echo
 		logger.Infow("OnMessage:", "messageType", messageType, "data", string(data))
-		c.WriteMessage(messageType, data)
-		for i := 0; i < 100; i++ {
-			c.WriteMessage(messageType, []byte("Fottiti coglione"))
+
+		var request Message
+
+		err := json.Unmarshal(data, &request)
+		if err != nil {
+			logger.Error("Unable to unmarshal JSON due to %s", err)
 		}
+
+		fmt.Println("Received message: ", request)
+
+		payload, err := json.Marshal(Message{
+			PlayerId: 10,
+			GameId:   20,
+			Payload: Payload{
+				StartRegionId: 100,
+				EndRegionId:   200,
+				NumTroops:     99,
+			},
+		})
+		if err != nil {
+			logger.Error("Unable to marshal JSON due to %s", err)
+		}
+		c.WriteMessage(messageType, payload)
 	})
+
 	u.OnClose(func(c *websocket.Conn, err error) {
 		logger.Infow("OnClose:", "remoteAddress", c.RemoteAddr().String(), "error", err)
 	})
+
 	return u
 }
