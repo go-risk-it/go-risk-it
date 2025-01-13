752c3bcd66ae550cf33a594faacdcad9f2e4bb3a
Giovanni Zotta <giovanni.zotta@booking.com>
2025-01-13 21:06:02 +0100

    close connection when attempting to connect to invalid game

--- internal/web/ws/connection/manager.go
+++ internal/web/ws/connection/manager.go
@@ -2,8 +2,13 @@ package connection
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 
 	"github.com/go-risk-it/go-risk-it/internal/ctx"
+	"github.com/go-risk-it/go-risk-it/internal/data/sqlc"
+	"github.com/go-risk-it/go-risk-it/internal/logic/game/player"
+	"github.com/go-risk-it/go-risk-it/internal/logic/game/state"
 	"github.com/go-risk-it/go-risk-it/internal/logic/signals"
 	upgradablerwmutex "github.com/go-risk-it/go-risk-it/internal/upgradablerw_mutex"
 	"github.com/lesismal/nbio/nbhttp/websocket"
@@ -18,6 +23,8 @@ type Manager interface {
 
 type ManagerImpl struct {
 	upgradablerwmutex.UpgradableRWMutex
+	gameStateService      state.Service
+	playerService         player.Service
 	gameConnections       map[int64]*playerConnections
 	playerConnectedSignal signals.PlayerConnectedSignal
 }
@@ -28,8 +35,14 @@ func (m *ManagerImpl) GetConnectedPlayers(ctx ctx.GameContext) []string {
 
 var _ Manager = (*ManagerImpl)(nil)
 
-func NewManager(playerConnectedSignal signals.PlayerConnectedSignal) *ManagerImpl {
+func NewManager(
+	gameStateService state.Service,
+	playerService player.Service,
+	playerConnectedSignal signals.PlayerConnectedSignal,
+) *ManagerImpl {
 	return &ManagerImpl{
+		gameStateService:      gameStateService,
+		playerService:         playerService,
 		gameConnections:       make(map[int64]*playerConnections),
 		playerConnectedSignal: playerConnectedSignal,
 	}
@@ -40,9 +53,54 @@ func (m *ManagerImpl) Broadcast(ctx ctx.GameContext, message json.RawMessage) {
 }
 
 func (m *ManagerImpl) ConnectPlayer(ctx ctx.GameContext, connection *websocket.Conn) {
+	ctx.Log().Infow("connecting player")
+
+	if err := m.validateConnectionAttempt(ctx); err != nil {
+		ctx.Log().Debugw("failed to validate connection attempt", "error", err)
+
+		err = connection.WriteClose(1003, "failed to validate connection attempt")
+		if err != nil {
+			ctx.Log().Errorw("failed to close websocket connection", "error", err)
+
+			return
+		}
+
+		return
+	}
+
 	m.playerConnections(ctx).ConnectPlayer(ctx, connection)
 
 	m.playerConnectedSignal.Emit(ctx, signals.PlayerConnectedData{})
+}
+
+func (m *ManagerImpl) validateConnectionAttempt(ctx ctx.GameContext) error {
+	gameState, err := m.gameStateService.GetGameState(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get game state: %w", err)
+	}
+
+	ctx.Log().Debugw("game state", "state", gameState)
+
+	players, err := m.playerService.GetPlayersState(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get player state: %w", err)
+	}
+
+	if !userIsParticipatingInGame(ctx, players) {
+		return errors.New("user not in game")
+	}
+
+	return nil
+}
+
+func userIsParticipatingInGame(ctx ctx.GameContext, players []sqlc.GetPlayersStateRow) bool {
+	for _, player := range players {
+		if player.UserID == ctx.UserID() {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (m *ManagerImpl) WriteMessage(ctx ctx.GameContext, message json.RawMessage) {
