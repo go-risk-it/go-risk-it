e0a8dec955566b61fbef58e4216f682c169677eb
Giovanni Zotta <giovanni.zotta@booking.com>
2025-01-19 23:30:48 +0100

    update cors middleware

--- internal/web/middleware/cors.go
+++ internal/web/middleware/cors.go
@@ -29,7 +29,7 @@ func (m *CorsMiddlewareImpl) Wrap(routeToWrap route.Route) route.Route {
 			writer.Header().Set("Access-Control-Allow-Origin", "*")
 			writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			writer.Header().
-				Set("Access-Control-Allow-Headers", "Content-MissionType, Authorization")
+				Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 			if request.Method == http.MethodOptions {
 				writer.WriteHeader(http.StatusOK)
